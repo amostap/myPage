2e785bb3f8edd29b9edecf59ce21f76cff2957ac
Alexandr Ostapenko <alexandr.amostap@gmail.com>
2017-03-05 22:31:53 +0200

    remove tests, lol, rewrite to function

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="main">
-          Hello World_
-        </div>
-        <div className="footer">
-          <a target="_blank" href="https://github.com/amostap">git</a>
-          <a target="_blank" href="https://www.linkedin.com/in/amostap/">linkedin</a>
-          <a target="_blank" href="https://twitter.com/amostap">twitter</a>
-          <a target="_blank" href="https://www.instagram.com/amostap/">instagram</a>
-          <a target="_blank" href="https://vk.com/amostap">vk</a>
-          <a target="_blank" href="skype:amgibson94">skype</a>
-          <a href="mailto:alexandr.amostap@gmail.com?Subject=Hello%20again" target="_top">email</a>
-        </div>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="App">
+    <div className="main">
+      Hello World_
+    </div>
+    <div className="footer">
+      <a target="_blank" href="https://github.com/amostap">git</a>
+      <a target="_blank" href="https://www.linkedin.com/in/amostap/">linkedin</a>
+      <a target="_blank" href="https://twitter.com/amostap">twitter</a>
+      <a target="_blank" href="https://www.instagram.com/amostap/">instagram</a>
+      <a target="_blank" href="https://vk.com/amostap">vk</a>
+      <a target="_blank" href="skype:amgibson94">skype</a>
+      <a href="mailto:alexandr.amostap@gmail.com?Subject=Hello%20World" target="_top">email</a>
+    </div>
+  </div>
+);
 
 export default App;
