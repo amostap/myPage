0acfaf8e7daed4c634060b7e07a076300f6fd7a9
Alexandr Ostapenko <alexandr.amostap@gmail.com>
2017-03-05 22:22:54 +0200

    v001

--- src/App.js
+++ src/App.js
@@ -5,7 +5,18 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        Hello World
+        <div className="main">
+          Hello World_
+        </div>
+        <div className="footer">
+          <a target="_blank" href="https://github.com/amostap">git</a>
+          <a target="_blank" href="https://www.linkedin.com/in/amostap/">linkedin</a>
+          <a target="_blank" href="https://twitter.com/amostap">twitter</a>
+          <a target="_blank" href="https://www.instagram.com/amostap/">instagram</a>
+          <a target="_blank" href="https://vk.com/amostap">vk</a>
+          <a target="_blank" href="skype:amgibson94">skype</a>
+          <a href="mailto:alexandr.amostap@gmail.com?Subject=Hello%20again" target="_top">email</a>
+        </div>
       </div>
     );
   }
