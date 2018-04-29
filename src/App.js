61f195891602c0e1652a48aea26b2627ebac5601
Alexandr Ostapenko <alexandr.amostap@gmail.com>
2018-04-30 00:44:39 +0300

    :D

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React, { Component, Fragment } from 'react';
+import React, { Fragment } from 'react';
 
 const LINKS = [
-  { name: 'git', href: 'https://github.com/amostap' },
+  { name: 'github', href: 'https://github.com/amostap' },
   { name: 'linkedin', href: 'https://www.linkedin.com/in/amostap' },
   { name: 'twitter', href: 'https://twitter.com/amostap' },
+  { name: 'codewars', href: 'https://www.codewars.com/users/amostap' },
   { name: 'instagram', href: 'https://www.instagram.com/amostap' },
   { name: 'vk', href: 'https://vk.com/amostap' },
   { name: 'upwork', href: 'https://www.upwork.com/o/profiles/users/_~0180e11d0eaedb0e7c' },
@@ -11,47 +12,22 @@ const LINKS = [
   { name: 'email', href: 'mailto:alexandr.amostap@gmail.com?Subject=Hello%20World' }
 ];
 
-export default class App extends Component {
-  state = {
-    date: new Date()
-  }
+const App = () => (
+  <Fragment>
+    <header className="header">
+      <div>
+        Hello World
+        <span className="blinking-cursor">|</span>
+      </div>
+    </header>
+    <footer className="footer">
+      {
+        LINKS.map((link, index) => (
+          <a key={index} className={link.name} target="_blank" href={link.href}>{link.name}</a>
+        ))
+      }
+    </footer>
+  </Fragment>
+);
 
-  componentDidMount() {
-    this.timerID = setInterval(
-      () => this.tick(),
-      1000
-    );
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.timerID);
-  }
-
-  tick() {
-    this.setState({
-      date: new Date()
-    });
-  }
-
-  render() {
-    const { date } = this.state;
-
-    return (
-      <Fragment>
-        <header className="header" style={ { color: `rgb(${date.getHours() * 10}, ${date.getMinutes() * 4}, ${date.getSeconds() * 4})` } }>
-          <div>
-            Hello World
-            <span class="blinking-cursor">|</span>
-          </div>
-        </header>
-        <footer className="footer">
-          {
-            LINKS.map(link => (
-              <a className={link.name} target="_blank" href={link.href}>{link.name}</a>
-            ))
-          }
-        </footer>
-      </Fragment>
-    );
-  }
-}
+export default App;
